66cb334550454b83ebeb5b55b0d40a799e7ac36a
Hristo Dimitrov <hristodimitrov9119@gmail.com>
2019-05-25 17:59:20 +0300

    Fix redis configuration

--- src/main/java/com/bioinfo/contacts/config/RedisConfig.java
+++ src/main/java/com/bioinfo/contacts/config/RedisConfig.java
@@ -1,8 +1,8 @@
 package com.bioinfo.contacts.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -10,18 +10,13 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    LettuceConnectionFactory lettuceConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("192.168.99.100");
-        configuration.setPort(6379);
-        return new LettuceConnectionFactory(configuration);
-    }
+    @Autowired
+    LettuceConnectionFactory lettuceConnectionFactory;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-        template.setConnectionFactory(lettuceConnectionFactory());
+        template.setConnectionFactory(lettuceConnectionFactory);
         template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
         return template;
     }
